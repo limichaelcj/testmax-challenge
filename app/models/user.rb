60379ea30081642ba9e432418bd8c83612e5a5cc
Michael Li <mcli.893@gmail.com>
2020-09-09 11:29:30 -0700

    configure form validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, format: { with: /\A([a-z](\-[a-z]|[\.\,\'])?[ \t]*)+\z/i, message: "Name is invalid or too short." }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email address." }
-  validates :phone, format: { with: /[0-9]{3}-?[0-9]{3}-?[0-9]{4}/, message: "Invalid phone number."}
+  validates :name, presence: true, format: { with: /\A([a-z](\-[a-z]|[\.\,\'])?[ \t]*){2,}\z/i, message: "is invalid or too short." }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "address is invalid." }
+  validates :phone, format: { with: /[0-9]{3}-?[0-9]{3}-?[0-9]{4}/, message: "number is invalid" }
 end
