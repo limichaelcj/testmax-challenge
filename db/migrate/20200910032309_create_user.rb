d54fd06f70ff165e05ddf4316b882ed0bcb903b4
Michael Li <mcli.893@gmail.com>
2020-09-09 20:27:47 -0700

    configure database, user schema, inflections for production env

--- db/migrate/20200910032309_create_user.rb
+++ db/migrate/20200910032309_create_user.rb
@@ -1,10 +1,11 @@
 class CreateUser < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
+    create_table :user do |t|
       t.string :first_name
       t.string :last_name
       t.string :email
       t.string :pnum
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_015217) do
+ActiveRecord::Schema.define(version: 2020_09_10_032309) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "user", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
     t.string "pnum"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
